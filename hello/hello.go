fed3d36093abaa93d871129dc95ceaeac06d17ae
depthzer0 <carngarn@gmail.com>
2017-03-28 15:43:53 +0300

    hello

---
+++ hello/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import(
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main(){
+	
+	who := "anybody"
+
+	if len(os.Args) > 1 {
+		who = strings.Join(os.Args[1:], " ")
+	}
+
+	fmt.Println("Hello, " + who)
+}
