ef869274c262e83b344a14a03ccddfb3a6fbc4f1
depthzer0 <carngarn@gmail.com>
2017-04-24 08:40:50 +0300

    stt

--- stacker/stacker.go
+++ stacker/stacker.go
@@ -10,6 +10,8 @@ func main()  {
 	var newStack stack.Stack
 	newStack.Push(123)
 	newStack.Push("asd")
+	newStack.Push(-888)
+	newStack.Push([]string{"asf", "2r23r23r", "asdqc1122121"})
 
 	fmt.Println(newStack)
 }
