1739fd7defb7773e4983b2667d906b3f2f4fd0b4
depthzer0 <carngarn@gmail.com>
2017-06-19 08:38:32 +0300

    st_3

--- stacker/stack/stack.go
+++ stacker/stack/stack.go
@@ -1,7 +1,14 @@
 package stack
 
+
+
 type Stack []interface{}
 
 func (stack *Stack) Push (x interface{}){
 	*stack = append(*stack, x)
+}
+
+func (stack *Stack) Pop () interface{}{
+	//*stack = append(*stack, x)
+	return 1
 }
